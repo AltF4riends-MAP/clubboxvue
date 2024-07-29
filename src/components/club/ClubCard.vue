9adb5a9c0ea2d1d9143bec80dc4c724763be450d
Moataz <mohammed08@graduate.utm.my>
2024-07-29 14:04:24 +0800

    few changes

--- src/components/club/ClubCard.vue
+++ src/components/club/ClubCard.vue
@@ -56,72 +56,61 @@ export default {
   }
 };
 </script>
-  
-  <style scoped>
-  .club-card {
-    border: 1px solid #ddd;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    margin: 10px;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    overflow: hidden;
-    transition: box-shadow 0.3s;
-    background-color: white;
-    width: 20%;
-    height: 60vh;
-  }
-  
-  .club-card:hover {
-    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-  }
-  
-  .club-image {
-    width: 15vw;
-    height: 30vh;
-    border-bottom: 1px solid #ddd;
-  }
-  
-  .club-details {
-    padding: 15px;
-    text-align: center;
-  }
-  
-  .club-title {
-    font-size: 1.3em;
-    margin: 10px 0;
-  }
-  
-  .club-date {
-    color: gray;
-    margin-bottom: 10px;
-  }
-  
-  .club-description {
-    font-size: 1em;
-    color: #333;
-  }
-  
-  .club-price {
-    font-size: 1em;
-    color: #333;
-  }
-  
-  button {
-    margin-top: 10px;
-    padding: 10px 20px;
-    background-color: maroon;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-  }
-  
-  button:hover {
-    background-color: darkgreen;
-  }
-  </style>
-  
+
+<style scoped>
+.club-card {
+  border: 1px solid #ddd;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 10px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+  transition: box-shadow 0.3s;
+  background-color: white;
+  width: 20%;
+  height: 60vh;
+}
+
+.club-card:hover {
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+}
+
+.club-image {
+  width: 15vw;
+  height: 30vh;
+  border-bottom: 1px solid #ddd;
+}
+
+.club-details {
+  padding: 15px;
+  text-align: center;
+}
+
+.club-title {
+  font-size: 1.3em;
+  margin: 10px 0;
+}
+
+.club-description {
+  font-size: 1em;
+  color: #333;
+}
+
+button {
+  margin-top: 10px;
+  padding: 10px 20px;
+  background-color: maroon;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+button:hover {
+  background-color: darkgreen;
+}
+</style>
